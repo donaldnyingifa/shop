68435ee7332f3be512d3615bc0f2bcf7117465ac
Donald Nyingifa <donaldnyingifa@gmail.com>
2019-09-13 21:44:12 +0100

    created project with create react app and added files for homepage

---
+++ src/Homepage/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import './home.css'
+export default function Home() {
+    return (
+        <div className='homepage'>
+            <div className='directory-menu'>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>SHIRTS</h1>
+                        <span className='subtile'>SHOP NOW</span>
+                    </div>
+                </div>
+
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>PANTS</h1>
+                        <span className='subtile'>SHOP NOW</span>
+                    </div>
+                </div>
+
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>SHOES</h1>
+                        <span className='subtile'>SHOP NOW</span>
+                    </div>
+                </div>
+
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>MENS</h1>
+                        <span className='subtile'>SHOP NOW</span>
+                    </div>
+                </div>
+
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>WOMENS</h1>
+                        <span className='subtile'>SHOP NOW</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
