b1d279273ab111f66befe6e8139359df231f6164
Donald Nyingifa <donaldnyingifa@gmail.com>
2019-09-14 07:07:30 +0100

    boken homepage into components, styled menu-item

---
+++ src/pages/Homepage/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './home.scss'
+
+import Directory from '../../components/directory'
+export default function Home() {
+    return (
+        <div className='homepage'>
+            < Directory />
+        </div>
+    )
+}
