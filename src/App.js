9f52f8073d29e7538a970ded432addc873090480
Donald Nyingifa <donaldnyingifa@gmail.com>
2019-09-15 07:43:06 +0100

    added shop page, collection-preview component-item component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 
-import Home from './pages/Homepage'
+import Home from './pages/HomePage'
+import ShopPage from './pages/ShopPage'
 
 function App() { 
   return (
     <div className="App">
       <Switch>
        <Route exact path='/' component={Home} />
+       <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
