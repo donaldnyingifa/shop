39a145b6c87074edfe5f62d766c58f1d176084b7
Donald Nyingifa <donaldnyingifa@gmail.com>
2019-09-15 12:52:13 +0100

    added header component and links to shop and master

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './App.css';
 
 import Home from './pages/HomePage'
 import ShopPage from './pages/ShopPage'
+import Header from './components/header'
 
 function App() { 
   return (
     <div className="App">
+      <Header />
       <Switch>
        <Route exact path='/' component={Home} />
        <Route exact path='/shop' component={ShopPage} />
