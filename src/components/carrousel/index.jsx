75ded6f17854762614b2ce473dfb83a86e2db779
Donald Nyingifa <donaldnyingifa@gmail.com>
2019-10-12 15:30:13 +0100

    changed carrousel, updated header

--- src/components/carrousel/index.jsx
+++ src/components/carrousel/index.jsx
@@ -1,14 +1,93 @@
-import React, { Component } from 'react'
-import './slider.scss'
-import image1 from '../../assets/kids.jpg'
-import image2 from '../../assets/everton.jpg'
-import image3 from '../../assets/resort.jpg'
-import BackgroundSlideshow from 'react-background-slideshow'
- 
-export default class Carrousel extends Component {
-  render () {
+import React from "react";
+
+import Carousel from "react-multi-carousel";
+import classNames from "classnames";
+import Avatar from "@material-ui/core/Avatar";
+
+import articles from "./data.json";
+import Card from "./card";
+import "./carousel-with-custom-dots.css";
+
+const responsive = {
+  desktop: {
+    breakpoint: {
+      max: 3000,
+      min: 1024
+    },
+    items: 3,
+    slidesToSlide: 2,
+    partialVisibilityGutter: 40
+  },
+  mobile: {
+    breakpoint: {
+      max: 464,
+      min: 0
+    },
+    items: 2,
+    slidesToSlide: 2,
+    partialVisibilityGutter: 30
+  },
+  tablet: {
+    breakpoint: {
+      max: 1024,
+      min: 200
+    },
+    items: 1,
+    slidesToSlide: 1,
+    partialVisibilityGutter: 30
+  }
+};
+
+const ArticleCard = ({ imageUrl, link, title }) => {
+  return <Card link={link} image={imageUrl} headline={title} />;
+};
+
+class CarouselWithCustomDots extends React.PureComponent {
+  render() {
+    const { deviceType } = this.props;
+    const children = articles
+      .slice(0, 6)
+      .map(article => <ArticleCard key={article.title} {...article} />);
+
+    const images = articles.map(article => (
+      <Avatar key={article.title} src={article.imageUrl} />
+    ));
+
+    const CustomDot = ({
+      index,
+      onClick,
+      active
+    }) => {
+      return (
+        <button
+          onClick={e => {
+            onClick();
+            e.preventDefault();
+          }}
+          className={classNames("custom-dot", {
+            "custom-dot--active": active
+          })}
+        >
+          {React.Children.toArray(images)[index]}
+        </button>
+      );
+    };
     return (
-        <BackgroundSlideshow images={[ image3, image2, image1 ]} />
-    )
+      <Carousel
+        showDots
+        deviceType={deviceType}
+        ssr
+        slidesToSlide={1}
+        containerClass="carousel-with-custom-dots"
+        responsive={responsive}
+        partialVisbile
+        infinite
+        customDot={<CustomDot />}
+      >
+        {children}
+      </Carousel>
+    );
   }
 }
+
+export default CarouselWithCustomDots;
--- src/pages/ShopPage/index.jsx
+++ src/pages/ShopPage/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SHOP_DATA from './SHOP_DATA'
-
+import Footer from '../../components/footer'
 import CollectionPreview from '../../components/collection-preview'
 import { id } from 'postcss-selector-parser';
 
@@ -16,11 +16,15 @@ class ShopPage extends React.Component {
     render() {
         const {collections} = this.state;
         return (
-            <div className='shop-page'>
-                {collections.map(({ id, ...otherCollectionProps}) => (
-                    <CollectionPreview key={id} {...otherCollectionProps} />
-                ))}
+            <div>
+                <div className='shop-page' style={{padding: '20px 80px'}}>
+                    {collections.map(({ id, ...otherCollectionProps}) => (
+                        <CollectionPreview key={id} {...otherCollectionProps} />
+                    ))}
+                </div>
+                <Footer />
             </div>
+            
         )
     }
 }
